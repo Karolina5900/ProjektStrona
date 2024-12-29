c0c92ab133ec34726b5581a014a85142fe8779f4
Karolina5900 <karolina.rosa.inf@gmail.com>
2024-12-29 13:20:43 +0100

    dodanie całego app

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Modele
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    ads = db.relationship('Ad', backref='author', lazy=True)
+
+class Ad(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+# Ruty
+@app.route('/')
+def index():
+    ads = Ad.query.all()
+    return render_template('index.html', ads=ads)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = password  # W produkcji należy użyć bcrypt do haszowania
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:  # Porównanie w produkcji po haszowaniu
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/add_ad', methods=['GET', 'POST'])
+@login_required
+def add_ad():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        category = request.form['category']
+        ad = Ad(title=title, description=description, category=category, author=current_user)
+        db.session.add(ad)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_ad.html')
+
+@app.route('/my_ads')
+@login_required
+def my_ads():
+    ads = Ad.query.filter_by(user_id=current_user.id).all()
+    return render_template('my_ads.html', ads=ads)
+
+if __name__ == '__main__':
+    app.run(debug=True)
